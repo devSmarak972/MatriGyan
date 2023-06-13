e2550eef49993d82351512cb056ce6fac35c0bb0
Anish Datta <anishd3139@gmail.com>
2023-06-13 19:17:49 +0530

    restructured code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-import Landing from "./pages/LoginSignup/LoginSignup";
+import LoginSignup from "./pages/LoginSignup/LoginSignup";
 import LandingPage from "./pages/LandingPage";
 function App() {
   return (
@@ -8,8 +8,8 @@ function App() {
       <div>
         <Routes>
           <Route exact path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Landing />} />
-          <Route path="/signup" element={<Landing />} />
+          <Route path="/login" element={<LoginSignup />} />
+          <Route path="/signup" element={<LoginSignup />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </div>
