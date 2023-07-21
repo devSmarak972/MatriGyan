62be64656fb7d7de4a195d6e420a8d602b435cc4
DevOp972 <100531674+devSmarak972@users.noreply.github.com>
2023-07-21 06:28:56 +0530

    setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -49,7 +49,7 @@ ALLOWED_HOSTS = [
     'localhost',
 ],
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
+    'http://localhost:3000','https://matrigyan.com'
 ]
 
 # Email
@@ -79,7 +79,7 @@ INSTALLED_APPS = [
 
 ]
 
-ALLOWED_HOSTS=['http://localhost:3000',"*"]
+ALLOWED_HOSTS=['http://localhost:3000','https://matrigyan.com'"*"]
 
 # CORS_ORIGIN_ALLOW_ALL = True
 # CORS_ALLOW_ALL_ORIGINS = True
@@ -102,18 +102,18 @@ CSRF_COOKIE_NAME="csrftoken"
 CORS_EXPOSE_HEADERS=["*","Cookie"]
 CORS_ALLOW_ALL_ORIGINS  = True
 CORS_ORIGIN_WHITELIST = (
-       'http://localhost:3000',
+       'http://localhost:3000','https://matrigyan.com'
 )
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
 "https://domain.com",
 "https://api.domain.com",
-"http://localhost:3000",
+'http://localhost:3000','https://matrigyan.com',
 "http://127.0.0.1:3000"
 ]# If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
 CORS_ALLOWED_ORIGIN_REGEXES = [
-    'http://localhost:3000',
+    'http://localhost:3000','https://matrigyan.com'
 ]
 CORS_ALLOW_METHODS = [
 'DELETE',
@@ -231,5 +231,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # }
 
 # CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
+#     'http://localhost:3000','https://matrigyan.com'
 # ]
